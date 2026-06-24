870f2511dfa430e19f074ab33d88a80b86752c64
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2026-06-24 15:32:19 +0200

    fetch and ingest script

--- src/fetch_and_ingest.py
+++ src/fetch_and_ingest.py
@@ -1,26 +1,24 @@
+
+import os
+from dotenv import load_dotenv
 import requests
 from datetime import datetime, timezone
 from decimal import Decimal
 
+load_dotenv()
+
+symbols = os.getenv("SYMBOLS").split(",")
+interval = os.getenv("INTERVAL")
+start_date = os.getenv("START_DATE")
+
 BASE_URL = "https://api.binance.com/api/v3/klines"
 
-start = datetime(2020, 1, 1, tzinfo=timezone.utc)
+start = datetime.strptime(start_date, "%Y-%m-%d").replace(tzinfo=timezone.utc)
 start_ms = int(start.timestamp() * 1000)
 
-params = {
-    "symbol": "BTCUSDT",
-    "interval": "1w",
-    "startTime": start_ms,
-    "limit": 1000
-}
-
-response = requests.get(BASE_URL, params=params)
-response.raise_for_status()
-
-data = response.json()
-
-def parse_candle(raw: list) -> dict:
+def parse_candle(raw: list, symbol: str) -> dict:
     return {
+        "symbol": symbol,
         "open_time": datetime.fromtimestamp(raw[0] / 1000, tz=timezone.utc),
         "open_price": Decimal(raw[1]),
         "high_price": Decimal(raw[2]),
@@ -30,4 +28,15 @@ def parse_candle(raw: list) -> dict:
         "number_of_trades": int(raw[8])
     }
 
-print(parse_candle(data[0]))
+params = {
+    "interval": interval,
+    "startTime": start_ms,
+    "limit": 1000
+}
+
+for symbol in symbols:
+    params["symbol"] = symbol
+    response = requests.get(BASE_URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+    print(parse_candle(data[0], symbol))
