71b1db0e86121f27353b82a6a3c794ae07f5bf43
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2026-06-25 14:31:55 +0200

    initial fast api setup

--- src/fast_api.py
+++ src/fast_api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from src.fetch_and_ingest import get_connection
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/candles/{symbol}/latest")
+def get_latest_candle(symbol: str):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""--sql
+                        SELECT * FROM crypto_weekly_candles
+                        WHERE symbol = %s
+                        ORDER BY open_time DESC
+                        LIMIT 1
+                        """, (symbol,))
+            return cur.fetchone()
