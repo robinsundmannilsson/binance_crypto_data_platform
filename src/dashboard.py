c190318feb20a06a07a98502bdd87d3e6630ee74
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2026-06-26 10:38:13 +0200

    dashboard ux setup

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import requests
+import base64
+
+with open("assets/binance_logo.svg", "rb") as f:
+    binance_logo = base64.b64encode(f.read()).decode()
 
 symbols = {
     "Bitcoin (BTC)": "BTCUSDT",
@@ -15,8 +19,9 @@ st.set_page_config(
     layout="wide")
 
 st.title("Weekly Crypto Candlestick Data Dashboard")
-st.header("This dashboard displays weekly candlestick data for selected cryptocurrencies")
-st.subheader("Powered by Binance")
+st.subheader("This dashboard displays weekly candlestick data for selected cryptocurrencies")
 
+st.sidebar.markdown(f'Powered by <img src="data:image/svg+xml;base64,{binance_logo}" height="80" style="vertical-align: middle; margin-left: -12px; margin-top: -2px;">', unsafe_allow_html=True)
 st.sidebar.header("Filtering Options")
 st.sidebar.selectbox("Select Cryptocurrency Symbol", list(symbols.keys()), key="selected_symbol")
+
