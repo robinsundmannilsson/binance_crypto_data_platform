ac38ab1457d1ef94403e77414e29d63f79bed350
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2026-06-26 12:37:05 +0200

    updated binance logo on dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -21,7 +21,7 @@ st.set_page_config(
 st.title("Weekly Crypto Candlestick Data Dashboard")
 st.subheader("This dashboard displays weekly candlestick data for selected cryptocurrencies")
 
-st.sidebar.markdown(f'Powered by <img src="data:image/svg+xml;base64,{binance_logo}" height="80" style="vertical-align: middle; margin-left: -12px; margin-top: -2px;">', unsafe_allow_html=True)
+st.sidebar.markdown(f'Powered by <img src="data:image/svg+xml;base64,{binance_logo}" height="75" style="vertical-align: middle; margin-left: -12px; margin-top: -2px;">', unsafe_allow_html=True)
 st.sidebar.header("Filtering Options")
 st.sidebar.selectbox("Select Cryptocurrency Symbol", list(symbols.keys()), key="selected_symbol")
 
