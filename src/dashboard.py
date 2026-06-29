3591181f7897946afa2871e330336d1e16290988
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2026-06-29 13:03:40 +0200

    updated dashboard with exchange rates

--- src/dashboard.py
+++ src/dashboard.py
@@ -2,12 +2,15 @@ import streamlit as st
 import requests
 import base64
 import pandas as pd
-from datetime import datetime
+from datetime import date
 import plotly.graph_objects as go
-from numpy.random import default_rng as rng
 
 with open("assets/binance_logo.svg", "rb") as f:
     binance_logo = base64.b64encode(f.read()).decode()
+
+exchange_params = {"date": date.today().isoformat(), "base": "USD", "quotes": "EUR,SEK,NOK,DKK"}
+exchange_rates_response = requests.get("https://api.frankfurter.dev/v2/rates?", params=exchange_params).json()
+exchange_rates = {item["quote"]: item["rate"] for item in exchange_rates_response}
 
 st.set_page_config(
     page_title="Crypto Dashboard",
@@ -25,8 +28,11 @@ symbols = {
     "Cardano (ADA)": "ADAUSDT",
 }
 
-selected_symbol = st.sidebar.selectbox("Select Cryptocurrency Symbol", list(symbols.keys()), key="selected_symbol")
+selected_symbol = st.sidebar.selectbox("Select Cryptocurrency", list(symbols.keys()), key="selected_symbol")
 symbol = symbols[selected_symbol]
+
+selected_rate = st.sidebar.selectbox("Select Exchange Rate", ["USD", "EUR", "SEK", "NOK", "DKK"], key="selected_rate")
+rate = 1.0 if selected_rate == "USD" else exchange_rates[selected_rate]
 
 latest_price_response = requests.get(f"http://localhost:8000/candles/{symbol}/latest")
 latest_price_data = latest_price_response.json()
@@ -46,10 +52,10 @@ st.subheader("Explore historical statistics and daily candlestick charts for sel
 st.divider()
 
 st.subheader(f"Latest trading data for {selected_symbol}")
-st.caption(f"Showing latest trading data as of {latest_date}")
+st.caption(f"Showing latest trading data as of {latest_date} in {selected_rate}")
 
-close = float(latest_price_data["close_price"])
-open = float(latest_price_data["open_price"])
+close = round(float(latest_price_data["close_price"]) * rate, 2)
+open = round(float(latest_price_data["open_price"]) * rate, 2)
 delta_open = round((open - close) / open * 100, 2)
 delta_close = round((close - open) / open * 100, 2)
 
@@ -64,10 +70,10 @@ with col2:
 col1, col2 = st.columns(2)
 with col1:
     with st.container(border=True):
-        st.metric("**High Price**", float(latest_price_data["high_price"]))
+        st.metric("**High Price**", round(float(latest_price_data["high_price"]) * rate, 2))
 with col2:
     with st.container(border=True):
-        st.metric("**Low Price**", float(latest_price_data["low_price"]))
+        st.metric("**Low Price**", round(float(latest_price_data["low_price"]) * rate, 2))
 
 col1, col2 = st.columns(2)
 with col1:
@@ -80,28 +86,28 @@ with col2:
 st.divider()
 
 st.subheader(f"Historical Statistics for {selected_symbol}")
-st.caption(f"Historical price data for {selected_symbol} from {earliest_date} to {latest_date}")
+st.caption(f"Historical price data for {selected_symbol} from {earliest_date} to {latest_date} in {selected_rate}")
 
 col1, col2 = st.columns(2)
 with col1:
     with st.container(border=True):
-        st.metric("**All-Time High Price**", float(all_time_high))
+        st.metric("**All-Time High Price**", round(float(all_time_high) * rate, 2))
 with col2:
     with st.container(border=True):
-        st.metric(f"**All-Time Low Price**", float(all_time_low))
+        st.metric(f"**All-Time Low Price**", round(float(all_time_low) * rate, 2))
 
 with st.container(border=True): 
 
     st.subheader(f"Candlestick Chart for {selected_symbol}")
     fig = go.Figure(data=[go.Candlestick(x=df['open_time'],
-                    open=df['open_price'],
-                    high=df['high_price'],
-                    low=df['low_price'],
-                    close=df['close_price'])])
+                    open=df['open_price'].astype(float) * rate,
+                    high=df['high_price'].astype(float) * rate,
+                    low=df['low_price'].astype(float) * rate,
+                    close=df['close_price'].astype(float) * rate)])
 
     fig.update_layout(
-                    xaxis_title='Date',
-                    yaxis_title='Price (USDT)',
+                    xaxis_title="Date",
+                    yaxis_title=f"Price ({selected_rate})",
                     xaxis_rangeslider_visible=False)
 
     st.plotly_chart(fig, use_container_width=True)
