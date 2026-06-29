fe06c55445baabc7ab4a8daa316de041183513d4
Robin Sundman Nilsson <sundmanrobin@outlook.com>
2026-06-29 11:18:58 +0200

    updated dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -4,6 +4,7 @@ import base64
 import pandas as pd
 from datetime import datetime
 import plotly.graph_objects as go
+from numpy.random import default_rng as rng
 
 with open("assets/binance_logo.svg", "rb") as f:
     binance_logo = base64.b64encode(f.read()).decode()
@@ -34,45 +35,73 @@ historical_price_response = requests.get(f"http://localhost:8000/candles/{symbol
 historical_price_data = historical_price_response.json()
 df = pd.DataFrame(historical_price_data)
 
-st.title("Daily Crypto Candlestick Data Dashboard")
-st.subheader("This dashboard displays daily candlestick data for selected cryptocurrencies")
-
-with st.container(border=True):
-    st.subheader(f"Latest Daily Candlestick Data for {selected_symbol}")
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        with st.container(border=True):
-            st.metric("**Open Price**", float(latest_price_data["open_price"]))
-    with col2:
-        with st.container(border=True):
-            st.metric("**High Price**", float(latest_price_data["high_price"]))
-    with col3:
-        with st.container(border=True):
-            st.metric("**Low Price**", float(latest_price_data["low_price"]))
-    with col4:
-        with st.container(border=True):
-            st.metric("**Close Price**", float(latest_price_data["close_price"]))
-
-    col1, col2 = st.columns(2)
-    with col1:
-        with st.container(border=True):
-            st.metric("**Volume**", float(latest_price_data["volume"]))
-    with col2:
-        with st.container(border=True):
-            st.metric("**Number of Trades**", int(latest_price_data["number_of_trades"]))
-
-fig = go.Figure(data=[go.Candlestick(x=df['open_time'],
-                open=df['open_price'],
-                high=df['high_price'],
-                low=df['low_price'],
-                close=df['close_price'])])
-
-fig.update_layout(title=f'Daily Candlestick Chart for {selected_symbol}',
-                  xaxis_title='Date',
-                  yaxis_title='Price (USDT)',
-                  xaxis_rangeslider_visible=False)
-
-st.plotly_chart(fig, use_container_width=True)
-
 all_time_high = df["high_price"].astype(float).max()
-st.metric("**All-Time High Price**", float(all_time_high))
+all_time_low = df["low_price"].astype(float).min()
+earliest_date = pd.to_datetime(df["open_time"]).min().strftime("%Y-%m-%d")
+latest_date = pd.to_datetime(df["open_time"]).max().strftime("%Y-%m-%d")
+
+st.title("Crypto Dashboard")
+st.subheader("Explore historical statistics and daily candlestick charts for selected cryptocurrencies.")
+
+st.divider()
+
+st.subheader(f"Latest trading data for {selected_symbol}")
+st.caption(f"Showing latest trading data as of {latest_date}")
+
+close = float(latest_price_data["close_price"])
+open = float(latest_price_data["open_price"])
+delta_open = round((open - close) / open * 100, 2)
+delta_close = round((close - open) / open * 100, 2)
+
+col1, col2 = st.columns(2)
+with col1:
+    with st.container(border=True):
+        st.metric("**Open Price**", open, delta=f"{delta_open}%")
+with col2:
+    with st.container(border=True):
+        st.metric("**Close Price**", close, delta=f"{delta_close}%")
+
+col1, col2 = st.columns(2)
+with col1:
+    with st.container(border=True):
+        st.metric("**High Price**", float(latest_price_data["high_price"]))
+with col2:
+    with st.container(border=True):
+        st.metric("**Low Price**", float(latest_price_data["low_price"]))
+
+col1, col2 = st.columns(2)
+with col1:
+    with st.container(border=True):
+        st.metric("**Volume**", float(latest_price_data["volume"]))
+with col2:
+    with st.container(border=True):
+        st.metric("**Number of Trades**", int(latest_price_data["number_of_trades"]))
+
+st.divider()
+
+st.subheader(f"Historical Statistics for {selected_symbol}")
+st.caption(f"Historical price data for {selected_symbol} from {earliest_date} to {latest_date}")
+
+col1, col2 = st.columns(2)
+with col1:
+    with st.container(border=True):
+        st.metric("**All-Time High Price**", float(all_time_high))
+with col2:
+    with st.container(border=True):
+        st.metric(f"**All-Time Low Price**", float(all_time_low))
+
+with st.container(border=True): 
+
+    st.subheader(f"Candlestick Chart for {selected_symbol}")
+    fig = go.Figure(data=[go.Candlestick(x=df['open_time'],
+                    open=df['open_price'],
+                    high=df['high_price'],
+                    low=df['low_price'],
+                    close=df['close_price'])])
+
+    fig.update_layout(
+                    xaxis_title='Date',
+                    yaxis_title='Price (USDT)',
+                    xaxis_rangeslider_visible=False)
+
+    st.plotly_chart(fig, use_container_width=True)
